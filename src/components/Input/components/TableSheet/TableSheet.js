6a4d786798c5dea634673f863ecac242f5a9ff57
PTBYSR <paul.emechebe@gmail.com>
2022-07-14 00:03:54 +0100

    git fix message here

--- src/components/Input/components/TableSheet/TableSheet.js
+++ src/components/Input/components/TableSheet/TableSheet.js
@@ -1,33 +1,91 @@
-import React, { useState } from 'react'
-import TableItems from './TableItems'
+import React, { useState } from "react";
+
+function TableItems({ index, tableItem, onChangeItem }) {
+  return (
+    <>
+      <tr>
+        <td>
+          <input type="text" required />
+        </td>
+        <td>
+          <input
+            type="number"
+            value={tableItem.price}
+            onChange={(e) => onChangeItem(index, "price", e.target.value)}
+          />
+        </td>
+        <td>
+          <input
+            type="number"
+            value={tableItem.quantity}
+            onChange={(e) => onChangeItem(index, "quantity", e.target.value)}
+          />
+        </td>
+        <td>{Number(tableItem.price) * Number(tableItem.quantity)}</td>
+      </tr>
+    </>
+  );
+}
 
 function TableSheet() {
-  const [tableItem, setTableItem] = useState([1]);
-  
-  //adding a new table cell (table row)
+  const [tableItem, setTableItem] = useState([
+    {
+      price: 0,
+      quantity: 0
+    }
+  ]);
+
+  const onChangeItem = (index, type, value) => {
+    const newTable = tableItem.map((item, idx) => {
+      if (idx === index)
+        return {
+          ...item,
+          [type]: value
+        };
+      return item;
+    });
+    setTableItem(newTable);
+  };
+
   const addCell = () => {
-    setTableItem((t) => [...t, t + 1])
-  }
+    setTableItem((t) => [
+      ...t,
+      {
+        price: 0,
+        quantity: 0
+      }
+    ]);
+  };
+
+  const totalPrice = tableItem.reduce((acc, cur) => {
+    acc += Number(cur.price) * Number(cur.quantity);
+    return acc;
+  }, 0);
 
   return (
     <div>
       <table>
-          <thead>
-            <th>Item Description</th>
-            <th>Price</th>
-            <th>Qty.</th>
-            <th>Total</th>
-          </thead>
-            {
-              tableItem.map((tableItem, index, setItem) => {
-                return <TableItems key={index} tableItem={tableItem} setItem={setItem} addCell={addCell}/>
-              })
-            }
-        </table>
-        <button onClick={addCell}>+</button>
+        <thead>
+          <th>Item Description</th>
+          <th>Price</th>
+          <th>Qty.</th>
+          <th>Total</th>
+        </thead>
+        {tableItem.map((tableItem, index) => {
+          return (
+            <TableItems
+              // key={}
+              index={index}
+              tableItem={tableItem}
+              onChangeItem={onChangeItem}
+            />
+          );
+        })}
+      </table>
+      <button onClick={addCell}>+</button>
+      <div>Total: {totalPrice}</div>
     </div>
-  )
+  );
 }
 
-
-export default TableSheet
+export default TableSheet;
